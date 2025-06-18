b8da0930f729ee6a2c1b40c684afd05532bc5b84
vladelo <vlad.lakhtion@gmail.com>
2025-06-19 00:13:54 +0300

    Adding docstring

--- app/services/dish_service.py
+++ app/services/dish_service.py
@@ -1,24 +1,57 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
 from sqlalchemy.exc import SQLAlchemyError
+from typing import Sequence, Optional
 
 from app.models.dish import Dish
 from app.schemas.dish import DishCreate
-from typing import Sequence, Optional
 
 
 class DishService:
-    def __init__(self, session: AsyncSession):
+    def __init__(self, session: AsyncSession) -> None:
+        """
+        Инициализация сервиса блюд.
+
+        Args:
+            session (AsyncSession): Асинхронная сессия базы данных.
+        """
         self.session = session
 
     async def get_all(self) -> Sequence[Dish]:
+        """
+        Получить все блюда из базы данных.
+
+        Returns:
+            Sequence[Dish]: Список всех блюд.
+        """
         result = await self.session.execute(select(Dish))
         return result.scalars().all()
 
     async def get_by_id(self, dish_id: int) -> Optional[Dish]:
+        """
+        Получить блюдо по его идентификатору.
+
+        Args:
+            dish_id (int): Идентификатор блюда.
+
+        Returns:
+            Optional[Dish]: Объект блюда, если найден, иначе None.
+        """
         return await self.session.get(Dish, dish_id)
 
     async def create(self, dish_create: DishCreate) -> Dish:
+        """
+        Создать новое блюдо в базе данных.
+
+        Args:
+            dish_create (DishCreate): Данные для создания блюда.
+
+        Raises:
+            SQLAlchemyError: При ошибках при работе с базой данных.
+
+        Returns:
+            Dish: Созданное блюдо.
+        """
         dish = Dish(**dish_create.model_dump())
         self.session.add(dish)
         try:
@@ -30,6 +63,18 @@ class DishService:
         return dish
 
     async def delete(self, dish_id: int) -> bool:
+        """
+        Удалить блюдо по идентификатору.
+
+        Args:
+            dish_id (int): Идентификатор блюда для удаления.
+
+        Raises:
+            SQLAlchemyError: При ошибках при работе с базой данных.
+
+        Returns:
+            bool: True, если блюдо было удалено, False если не найдено.
+        """
         dish = await self.get_by_id(dish_id)
         if dish:
             await self.session.delete(dish)
