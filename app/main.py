b0e11891cdfb2f68963c8a53c91bff7cda0c53a5
vladelo <vlad.lakhtion@gmail.com>
2025-06-18 22:41:52 +0300

    Adding main.py

--- app/main.py
+++ app/main.py
@@ -1,3 +1,6 @@
 from fastapi import FastAPI
+from app.api.api import api_router
 
 app = FastAPI(title='FastAPI-Restaurant', description='Made by Vladislav Lahtionov')
+
+app.include_router(api_router, prefix="/api")
