4db03e3f37102389983c94c9d84c1b43cdacd057
vladelo <vlad.lakhtion@gmail.com>
2025-06-18 22:20:07 +0300

    Adding dish model

---
+++ app/models/dish.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, Integer, String, Float
+
+from app.core.database import Base
+
+
+class Dish(Base):
+    __tablename__ = "dishes"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, nullable=False, unique=True, index=True)
+    description = Column(String, nullable=True)
+    price = Column(Float, nullable=False)
+    category = Column(String, nullable=False)
