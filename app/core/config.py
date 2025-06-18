0a4868daa65be413c2562908395e8c416178f02b
vladelo <vlad.lakhtion@gmail.com>
2025-06-18 22:17:20 +0300

    Adding core module: config + database

---
+++ app/core/config.py
@@ -0,0 +1,13 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+# DB PostgreSQL
+DATABASE_USERNAME = os.getenv('DATABASE_USERNAME')
+DATABASE_PASSWORD = os.getenv('DATABASE_PASSWORD')
+DATABASE_HOST = os.getenv('DATABASE_HOST')
+DATABASE_PORT = int(os.getenv('DATABASE_PORT'))
+DATABASE_NAME = os.getenv('DATABASE_NAME')
+
+SQLALCHEMY_DATABASE_URL = f"postgresql://{DATABASE_USERNAME}:{DATABASE_PASSWORD}@{DATABASE_HOST}/{DATABASE_NAME}"
---
+++ app/core/database.py
@@ -0,0 +1,20 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+from app.core import config
+
+SQLALCHEMY_DATABASE_URL = config.SQLALCHEMY_DATABASE_URL
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
